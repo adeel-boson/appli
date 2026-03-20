f7c615641506ae2350f79f0eca906211b1305727
Adeel <muhammadadeel97531@gmail.com>
2026-03-20 18:09:36 +0500

    Feature: develop a landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,55 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="min-h-screen bg-purple-100  bg-[url('/landing-page-bg.jpg')] bg-cover bg-center px-4 py-4">
+      <div
+        className="w-[calc(100%-2rem)] mx-auto bg-green-900/30 px-5 py-2 rounded-sm backdrop-blur-xs
+      flex justify-between items-center
+      "
+      >
+        <h1 className="text-2xl font-bold font-mono text-white">Appli</h1>
+
+        <div className="flex gap-x-3">
+          <Link href="/login" className="text-white">
+            Login
+          </Link>
+
+          <span className="text-white">|</span>
+
+          <Link href="/register" className="text-white">
+            Register
+          </Link>
+        </div>
+      </div>
+
+      <div className="w-full">
+        <div
+          className="flex flex-col gap-y-2 bg-white/10 p-10 w-max max-w-[700px] rounded-sm absolute top-1/2 left-10 -translate-y-1/2 
+        backdrop-blur-xs shadow-xs shadow-green-900/10"
+        >
+          <div className="w-fit">
+            <h3 className="text-4xl text-green-900 animate-typing overflow-hidden whitespace-nowrap border-r-2 border-green-900">
+              Looking to hire? | Get hired?
+            </h3>
+          </div>
+          <p className="text-lg text-green-800">
+            Appli helps you find the right job or the right candidate for your
+            business. <br />
+            Whether you are a job seeker or a business owner, Appli is the
+            perfect platform for you.
           </p>
+          <div className="flex gap-x-2 mt-3">
+            <Button variant="outline" className="border-green-900 px-7 py-5">
+              Post a Job
+            </Button>
+            <Button variant="outline" className="border-green-900 px-7 py-5 ">
+              Find a Job
+            </Button>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
